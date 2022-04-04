198eb61fe19460bd9c05205cdfd7d7c703805cc8
toastloop <toastloop@gmail.com>
2022-04-04 11:03:02 -0600

    CompLab2

---
+++ CompLab2/Template.java
@@ -0,0 +1,67 @@
+
+public class AkbarPark {
+
+    public static void main(String[] args) {
+        //TODO
+    }
+
+    public static void welcomingMessage() {
+        //TODO
+        //This function displays a welcoming message.
+    }
+
+    public static boolean authenticator(){
+        //This function takes care of authentication (sign in/up).
+    }
+
+    public static boolean signIn(){
+        //TODO
+        //The function takes care of sign in action.
+    }
+
+    public static boolean signUp(){
+        //TODO
+        //This function takes care of sign up action.
+    }
+
+    public static boolean passwordChecker(String password) {
+        //TODO
+        //This function checks the password requirements.
+    }
+
+    public static boolean hasLowerCase(String password) {
+        //TODO
+        //This function checks if the string has a lower case letter.
+    }
+
+    public static boolean hasUpperCase(String password) {
+        //TODO
+        //This function checks if the string has an upper case letter.
+    }
+
+    public static boolean hasDigit(String password) {
+        //TODO
+        //This function checks if the string has a digit.
+    }
+
+    public static boolean hasSpecialChar(String password) {
+        //TODO
+        //This function checks if the string has a special character.
+    }
+
+    public static boolean hasEightChar(String password) {
+        //TODO
+        //This function checks if the string has at least eight characters
+    }
+
+    public static void displayMenu() {
+        //TODO
+        // This function displays the options in the menu
+    }
+
+    public static boolean isAllDigits(String str) {
+        //TODO
+        //this function checks if the credit card number is all in digits.
+    }
+
+}
